69346684291a6183ffc73ce8b174e0b64e754ac5
MrPowers <matthewkevinpowers@gmail.com>
2016-06-25 08:43:50 -0500

    Refactor the code and add a few comments so it's a little easier to follow

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,20 +4,30 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     lyrics_path = File.join(Rails.root, "song_data", @song.name.downcase + ".txt")
     lyrics = File.read(lyrics_path).split("\n")
+    @blankified_lyrics = processed_lyrics(lyrics)
+  end
+
+  private
+
+  def processed_lyrics(lyrics)
     lines_per_blank = 3
     counter = 1
-    @blankified_lyrics = []
+    blankified_lyrics = []
     lyrics.each do |lyric|
+      # Skip entire lines that begin with "SKIP "
       if lyric.start_with?("SKIP ")
         result = lyric
       else
         words = lyric.split(" ")
+        # Skip lines that are empty
         if words.empty?
           result = lyric
         else
+          # Skip lines when the counter isn't high enough
           if counter != lines_per_blank
             counter += 1
             result = lyric
+          # When the counter is high enough, add an input box to the line
           else
             counter = 1
             indices = words.each_index.select{|i| !words[i].start_with?("SKIP")}
@@ -27,8 +37,9 @@ class SongsController < ApplicationController
           end
         end
       end
-      @blankified_lyrics << result.gsub("SKIP", "").html_safe
+      blankified_lyrics << result.gsub("SKIP", "").html_safe
     end
+    blankified_lyrics
   end
 
 end
