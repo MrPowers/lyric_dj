6934babdea4e84184d5316fc202062a99604e4ae
djres88 <davidj.resnick@gmail.com>
2016-07-04 12:05:38 -0700

    Adding code for seed data (for when we want to tx over songs from current site)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,28 @@
 
 bieber = Artist.first_or_create(first_name: "Justin", last_name: "Bieber")
 song = Song.first_or_create(name: "Sorry", language: "english", youtube_id: "fRh_vgS2dFE", artist_id: bieber.id)
+
+# NOTE: For when we want to tx over all the songs from the current lyricDJ.
+require 'json'
+
+# Convert json to Ruby hash
+songs = JSON.parse(File.read('db/songs.json'))
+
+songs.each do |song|
+  artist = song.artist.split("-")
+  first_name = artist[0]
+  last_name = artist[1]
+  # But I'm actually thinking we should just make the artist be one field. No first/last names, like this:
+  # artist = song.artist.gsub("-", " ")
+  new_artist = Artist.create!(
+    first_name: first_name,
+    last_name: last_name
+  )
+  Song.create!(
+    name: song.name,
+    # I know this is an N+1 query and bad, but we're only doing it one time (when we seed).
+    artist_id: new_artist.id,
+    language: song.language,
+    youtube_id: song.youtube_id
+  )
+end
