4e798297f253f24d69aa09e449cdda6c70c7b897
djres88 <davidj.resnick@gmail.com>
2016-07-12 13:44:11 -0700

    Blogs controller and route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :songs, only: [:show]
-
   # Adding a static controller, per the devise instructions. We'll need one (or something similar) for the homepage, anyway.
   root 'static#root'
+
+  devise_for :users
+
+  resources :songs, only: [:show]
+
+  get 'como-aprender-ingles', to: 'blogs#about'
 
 end
