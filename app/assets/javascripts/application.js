394b89d5221b46fe98bd7ff1e040322566011b72
MrPowers <matthewkevinpowers@gmail.com>
2018-04-05 16:19:59 -0400

    Upgrade to Rails 5.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery.slick
+//= require rails-ujs
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # add config for Rails 5 upgrade per this GitHub thread
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
